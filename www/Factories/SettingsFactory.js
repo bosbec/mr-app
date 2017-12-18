db600b38b78889a996dacd01c7c0cac632648828
BJÖRNCOLLIANDER\Colliander <bjorn@bosbec.se>
2017-12-18 09:50:29 +0100

    0.3.42 pushwoosh v 7.0.7

--- www/Factories/SettingsFactory.js
+++ www/Factories/SettingsFactory.js
@@ -5,7 +5,7 @@
         function ($localStorage) {
 
             var appId = "com.bosbec.mr-app";
-            var appVersion = "0.3.41";
+            var appVersion = "0.3.42";
 
             var defaultNumberOfConversations = 15;
             var defaultNumberOfMessages = 20;
