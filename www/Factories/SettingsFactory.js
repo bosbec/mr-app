9f9fa69a4da872d50f5b23c19082de50885ab2db
BJÖRNCOLLIANDER\Colliander <bjorn@bosbec.se>
2018-05-21 16:39:05 +0200

    0.3.72 modal height fix

--- www/Factories/SettingsFactory.js
+++ www/Factories/SettingsFactory.js
@@ -5,7 +5,7 @@
         function ($localStorage) {
 
             var appId = "com.bosbec.mr-app";
-            var appVersion = "0.3.71";
+            var appVersion = "0.3.72";
             var appPushId = "64032-F5A58";
             //var appId = "se.bosbec.Messmr1";
             //var appVersion = "4.0.12";
